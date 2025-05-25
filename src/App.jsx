4a077ebb57200d502e4a79b22ca97c84e2060709
Sheetal <21q91a66e1@mrce.in>
2025-05-25 15:58:02 +0530

    asdasdas

--- src/App.jsx
+++ src/App.jsx
@@ -18,13 +18,7 @@ function App() {
       <div className='min-h-screen flex justify-center items-center'>
         <button className='bg-amber-300 border p-2 rounded-lg hover:cursor-pointer' onClick={() => HandleClickFromFirstButton()}>Click me</button>
         <button className='bg-amber-300 border p-2 rounded-lg hover:cursor-pointer' onClick={() => HandleClickFromFirstButton()}>Click me</button>
-<<<<<<< HEAD
         <div>This is the easyway</div>
-=======
-        <div>
-          Hello
-        </div>
->>>>>>> d08e64a8693544206abaf114fb7481496bd245ce
       </div>
       
     </div>
